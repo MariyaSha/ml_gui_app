dee79a43f020ad13b8f325f4aeba687d9758af83
Rohan Rustagi <110477025+RohanRusta21@users.noreply.github.com>
2023-12-07 18:31:26 +0000

    "Added Dockerfile to dockerized application"

--- finishedProject/classifier.py
+++ finishedProject/classifier.py
@@ -67,4 +67,4 @@ def on_change(state, var_name, var_val):
 app = Gui(page=index)
 
 if __name__ == "__main__":
-    app.run(use_reloader=True)
+    app.run(host='0.0.0.0', port=5000, use_reloader=True)
